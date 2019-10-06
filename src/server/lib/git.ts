7eb791ba94dd2aaa25a293dda3af1b33148152ca
Dmitry Shlensky <shld@list.ru>
2019-10-07 00:22:04 +0300

    Remove unused methods from git wrapper

--- src/server/lib/git.ts
+++ src/server/lib/git.ts
@@ -1,116 +1,10 @@
-import rimraf from 'rimraf';
-
 import { EOL } from 'os';
 import { promisify } from 'util';
-import { readdir } from 'fs';
-import { exec, spawn } from 'child_process';
+import child from 'child_process';
 
 import { TreeItem } from 'schema/Tree';
 
-const readdirAsync = promisify(readdir);
-const execAsync = promisify(exec);
-
-export async function getRepos(reposPath: string) {
-    const files = await readdirAsync(reposPath, { withFileTypes: true });
-    return files.filter(file => file.isDirectory()).map(file => file.name);
-}
-
-export async function cloneRepo(reposPath: string, repoUrl: string) {
-    const CLONE_COMMAND = `git --no-pager clone ${repoUrl}`;
-    const options = { cwd: reposPath, env: { GIT_TERMINAL_PROMPT: '0' } };
-
-    await execAsync(CLONE_COMMAND, options);
-}
-
-export async function removeRepo(repositoryPath: string) {
-    await promisify(rimraf)(repositoryPath);
-}
-
-function parseCommitLine(line: string) {
-    const [hash, created, author, ...message] = line.split('|');
-    return { hash, created, author, message: message.join('|') };
-}
-
-export async function getCommits(repoPath: string, branch: string) {
-    const GET_COMMITS_COMMAND = `git --no-pager log --pretty=format:"%H|%ad|%an|%s" --no-decorate --date=iso ${branch}`;
-
-    return new Promise((resolve, reject) => {
-        try {
-            const options = { cwd: repoPath };
-            exec(GET_COMMITS_COMMAND, options, function(error, stdout) {
-                if (error !== null) {
-                    reject(error);
-                    return;
-                }
-
-                const commits = stdout.split(EOL).map(parseCommitLine);
-                resolve(commits);
-            });
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
-
-async function getFileDiff(repoPath: string, branch: string, fileName: string) {
-    const GET_DIFF_COMMAND = `git --no-pager diff ${branch}^! ${fileName}`;
-
-    return new Promise((resolve, reject) => {
-        try {
-            const options = { cwd: repoPath };
-            exec(GET_DIFF_COMMAND, options, function(error, stdout) {
-                if (error !== null) {
-                    if (error instanceof RangeError) {
-                        resolve({ diff: 'This diff is too large', collapsed: true });
-                    }
-
-                    reject(error);
-                    return;
-                }
-
-                resolve({ diff: stdout });
-            });
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
-
-export async function getDiff(repoPath: string, branch: string) {
-    const GET_CHANGED_FILES_COMMAND = `git --no-pager diff --name-status ${branch}^!`;
-
-    return new Promise((resolve, reject) => {
-        try {
-            const options = { cwd: repoPath };
-            exec(GET_CHANGED_FILES_COMMAND, options, async function(error, stdout) {
-                if (error !== null) {
-                    reject(error);
-                    return;
-                }
-
-                const changedFiles = stdout.split(EOL).filter(fileName => !!fileName.trim());
-
-                Promise.all(
-                    changedFiles.map(async fileNameWithStatus => {
-                        const [status, fileName] = fileNameWithStatus.split('\t');
-
-                        const diffObject = status === 'D' ? {} : await getFileDiff(repoPath, branch, fileName);
-
-                        return {
-                            status,
-                            fileName,
-                            ...diffObject,
-                        };
-                    }),
-                )
-                    .then(resolve)
-                    .catch(reject);
-            });
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
+const execAsync = promisify(child.exec);
 
 interface ParsedTreeLine {
     mode: string;
@@ -175,5 +69,5 @@ export function getBlob(repoPath: string, branch: string, path: string) {
     const args = ['--no-pager', 'show', `${branch}:${path}`];
     const options = { cwd: repoPath };
 
-    return spawn('git', args, options);
+    return child.spawn('git', args, options);
 }
