d45397592da2fc8765e8ba5e96f8bc68e694e289
Dmitry Shlensky <shld@list.ru>
2019-10-06 20:26:41 +0300

    User bem-react for CodeTable component

--- src/components/CodeTable/CodeTable.tsx
+++ src/components/CodeTable/CodeTable.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import { cn } from '@bem-react/classname';
 
 import 'components/CodeTable/CodeTable.scss';
+
+const cnCodeTable = cn('CodeTable');
 
 interface CodeTableProps {
     content: string;
@@ -9,12 +12,12 @@ interface CodeTableProps {
 export function CodeTable({ content }: CodeTableProps) {
     const lines = content.split('\n');
     return (
-        <table className="CodeTable">
+        <table className={cnCodeTable()}>
             <tbody>
                 {lines.map((line, i) => (
-                    <tr className="CodeTable-Row" key={i}>
-                        <td className="CodeTable-LineNumber">{i + 1}</td>
-                        <td className="CodeTable-Code">{line}</td>
+                    <tr className={cnCodeTable('Row')} key={i}>
+                        <td className={cnCodeTable('LineNumber')}>{i + 1}</td>
+                        <td className={cnCodeTable('Code')}>{line}</td>
                     </tr>
                 ))}
             </tbody>
