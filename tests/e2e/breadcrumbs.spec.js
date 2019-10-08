4b85f574f91a925357c24199fd15026859f98b11
Dmitry Shlensky <shld@list.ru>
2019-10-08 18:48:23 +0300

    Add e2e tests for breadcrumbs

---
+++ tests/e2e/breadcrumbs.spec.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect;
+
+describe('Хлебные крошки', function() {
+    it('Отображают имя репозитория', function() {
+        return this.browser
+            .url('/first-repo')
+            .getText('.Breadcrumbs *')
+            .then(breadCrumb => {
+                expect(breadCrumb).to.eql('first-repo');
+            });
+    });
+
+    it('Отображают имя директории', function() {
+        return this.browser
+            .url('/first-repo/tree/master/dir1')
+            .getText('.Breadcrumbs *')
+            .then(breadCrumbs => {
+                expect(breadCrumbs).to.eql(['first-repo', 'dir1']);
+            });
+    });
+
+    it('Отображают имя файла', function() {
+        return this.browser
+            .url('/first-repo/blob/master/dir1/first-file.txt')
+            .getText('.Breadcrumbs *')
+            .then(breadCrumbs => {
+                expect(breadCrumbs).to.eql(['first-repo', 'dir1', 'first-file.txt']);
+            });
+    });
+});
