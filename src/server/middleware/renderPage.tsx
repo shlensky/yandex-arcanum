640813845c777b87feca965f221e2f083d4da2fa
Dmitry Shlensky <shld@list.ru>
2019-10-12 18:01:08 +0300

    Replace renderToStaticMarkup by renderToString

--- src/server/middleware/renderPage.tsx
+++ src/server/middleware/renderPage.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 
 import express from 'express';
 
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { App } from 'components/App/App@server';
 
 export function renderPage(): express.Handler {
     return (req: express.Request, res: express.Response) => {
-        let content = renderToStaticMarkup(<App state={req.state} url={req.url} />);
+        let content = renderToString(<App state={req.state} url={req.url} />);
 
         res.send('<!DOCTYPE html>' + content).end();
     };
